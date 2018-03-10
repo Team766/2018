fcaa9ad285259552a1a67ee3a7e44d4d8dbd63dd
maystify <maya@feedly.com>
2018-03-09 19:08:04 -0800

    Merge branch 'master' into auton-paths

--- 2018/src/com/team766/lib/CommandBase.java
+++ 2018/src/com/team766/lib/CommandBase.java
@@ -4,21 +4,24 @@ import interfaces.SubActor;
 
 import com.team766.robot.Actors.Drive.Drive;
 import com.team766.robot.Actors.Intake.Intake;
-import com.team766.robot.Actors.Arm.Arm;
+import com.team766.robot.Actors.Shoulder.Shoulder;
 import com.team766.robot.Actors.Climber.Climber;
+import com.team766.robot.Actors.Wrist.Wrist;
 
 public abstract class CommandBase implements SubActor{
 	
 	public static Drive Drive;
 	public static Intake Intake;
-	public static Arm Arm;
+	public static Shoulder Shoulder;
 	public static Climber Climber;
+	public static Wrist Wrist;
 	
 	public static void init(){
 		Drive = new Drive();
 		Intake = new Intake();
-		Arm = new Arm();
+		Shoulder = new Shoulder();
 		Climber = new Climber();
+		Wrist = new Wrist();
 	}
 
 }
--- 2018/src/com/team766/lib/Messages/ShoulderPIDMessage.java
+++ 2018/src/com/team766/lib/Messages/ShoulderPIDMessage.java
@@ -8,6 +8,7 @@ public class ShoulderPIDMessage implements Message{
 	//0,1,2 --> Down, Middle, Vertical
 	public ShoulderPIDMessage(int position){
 		this.position = position;
+		System.out.println("inside shoulder message");
 	}
 	
 	public int getDesiredPos(){
